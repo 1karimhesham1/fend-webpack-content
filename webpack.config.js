2e5a7b41046b0448e46b5db47922e8be45b694aa
karim <Git Hub>
2024-11-24 15:34:20 +0200

    Save progress on Webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
-const path = require('path') 
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-
-}
+  mode: 'development', // or 'production'
+  entry: './src/client/js/index.js', // Ensure this is the correct path to your entry file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
